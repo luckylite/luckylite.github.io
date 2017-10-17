6655f25dd5b313e8c30114b9ad151cfbe8bec8a1
luckylite <30803064+luckylite@users.noreply.github.com>
2017-10-17 23:08:16 +0300

    Test Commit

--- js/common.js
+++ js/common.js
@@ -1,5 +1,15 @@
 $(function() {
 
-	// Custom JS
+	$('.nav-sandwich').click(function() {
+
+		$('.nav-sandwich_content').fadeIn();
+
+	});
+
+	$('.sandwich-close').click(function() {
+
+		$('.nav-sandwich_content').fadeOut();
+
+	});
 
 });
