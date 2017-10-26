2f7fb6255d15258023699d6bda739162e9c1d3de
luckylite <30803064+luckylite@users.noreply.github.com>
2017-10-26 22:46:21 +0300

    commmmmitttt

--- js/common.js
+++ js/common.js
@@ -18,12 +18,32 @@ $(document).ready(function() {
 		nav: false
 	});
 
+	$('.mobile-slider-blocks').owlCarousel({
+		loop: true,
+		items: 1,
+		smartSpeed: 700,
+		nav: true,
+		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+	});
+
 	$('.top-line_create_progect_button a.button').click(function() {
 		$('.popap-start_progect').fadeIn();
 	});
 
 	$('.popap-window .exit-popap').click(function() {
 		$('.popap-start_progect').fadeOut();
+	});
+
+	$('#name_m').keyup(function() {
+		var name_m_val = $('#name_m').val();
+		if (name_m_val.length >= 5) {
+			$('.menu-content').fadeOut();
+			$('.popap-start_progect').fadeIn();
+			$('#name').val(name_m_val);
+			$('#name').focus();
+			$('#name_m').val('');
+			$('.menu-button .hamburger').removeClass('is-active');
+		}
 	});
 
 });
