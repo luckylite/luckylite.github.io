9f205cfad387e6569209c7ca40a2fc5d0f45c142
luckylite <30803064+luckylite@users.noreply.github.com>
2017-10-26 16:52:06 +0300

    Cooommit

--- js/common.js
+++ js/common.js
@@ -1,45 +1,19 @@
 $(document).ready(function() {
 
-	$('.button-start_progect').click(function() {
-		$('.popap-start_progect-container').fadeIn();
-	});
-
-	$('.popap-exit_button').click(function() {
-		$('.popap-start_progect-container').fadeOut();
-	});
-
-	$('.slider_content').slick({
-		dots: true,
-    customPaging: function(slick,index) {
-        return '<div class="slide-page_button"></div>';
-    }
-	});
-
 	$('.menu-button').click(function() {
-		$('.menu-content').fadeIn();
-	});
 
-	$('.menu-button-top_line').click(function() {
-		$('.menu-content').fadeIn();
-	});
-
-	$('.menu-exit').click(function() {
-		$('.menu-content').fadeOut();
-	});
-
-	$('.menu-exit-second').click(function() {
-		$('.menu-content').fadeOut();
-	});
-
-	$('#name_m').keyup(function() {
-		var name_m_val = $('#name_m').val();
-		if (name_m_val.length >= 5) {
+		if ($('.menu-button .hamburger').hasClass('is-active')) {
+			$('.menu-button .hamburger').removeClass('is-active');
 			$('.menu-content').fadeOut();
-			$('.popap-start_progect-container').fadeIn();
-			$('#name').val(name_m_val);
-			$('#name').focus();
-			$('#name_m').val('');
+		} else {
+			$('.menu-button .hamburger').addClass('is-active');
+			$('.menu-content').fadeIn();
 		}
+
+	});
+
+	$('.content-slider').owlCarousel({
+		items: 1
 	});
 
 });
