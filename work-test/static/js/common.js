da7ffbb75989a1386088d654dd0a30fa2d77e941
Vlad Kandyba <quit3.it@gmail.com>
2018-08-11 13:17:35 +0300

    asdsad

--- work-test/static/js/common.js
+++ work-test/static/js/common.js
@@ -1,4 +1,12 @@
+function GoTo(link) {
+	window.open(link.replace("_","http://"));
+}
+
 $(document).ready(function() {
+
+	$('#contacts .contacts-close').click(function() {
+		$('#contacts .contacts-container').fadeOut();
+	});
 
 	$('.mobile-menu_button').click(function() {
 		$('.mobile-menu_list').toggle("slow");
