eef12b2f1db5024ed55762273028bfeebc8e0d40
Vlad Ivanov <luckylitestudio@gmail.com>
2019-02-21 20:59:02 +0200

    Revision

--- lorips/assets/js/main.js
+++ lorips/assets/js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+	$(window).scroll(function() {
+
+		if ($(window).scrollTop() > 0) {
+			$('.top-line').addClass('fixed');
+		} else {
+			$('.top-line').removeClass('fixed');
+		}
+
+	});
+
+
+
+
+
+
+
 	$('.input-select').click(function() {
 		$(this).toggleClass('active');
 	});
