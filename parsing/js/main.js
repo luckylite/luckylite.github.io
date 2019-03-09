3b44be67ca5a6a686758bba19b63dc60375737ce
Vlad Kandyba <quit3.it@gmail.com>
2019-03-09 19:57:41 +0200

    asdasf

--- parsing/js/main.js
+++ parsing/js/main.js
@@ -4,4 +4,34 @@ $(document).ready(function() {
 		$('.mobile-menu').toggleClass('active');
 	});
 
+
+
+	$('.select-menu').click(function() {
+		$(this).toggleClass('active');
+	});
+
+	$('.menu-list li').click(function() {
+		var val = $(this).html();
+		var menu = $(this).closest('.select-menu');
+		var selected = menu.children('.selected');
+
+		selected.html(val);
+	});
+
+
+	$('.checkbox').click(function() {
+		$(this).toggleClass('active');
+	});
+
+
+	bodyPadding();
+
+	$(window).resize(function() {
+		bodyPadding();
+	});
+
 });
+
+function bodyPadding() {
+	$('body').css('padding-bottom', $('.footer').height() + 'px');
+}
