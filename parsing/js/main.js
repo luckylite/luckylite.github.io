14c4f0502b843610bcd0706ea99b82b28ff94e71
Vlad Kandyba <quit3.it@gmail.com>
2019-03-10 17:42:43 +0200

    asdasd

--- parsing/js/main.js
+++ parsing/js/main.js
@@ -23,15 +23,40 @@ $(document).ready(function() {
 		$(this).toggleClass('active');
 	});
 
+	$('.add-button').click(function() {
+		var parent = $(this).closest('.add-box');
+		var form = parent.children('.add-form');
+
+		$(this).toggleClass('active');
+		$(form).toggleClass('active');
+	});
+
+	$('.add-form button').click(function() {
+		var parent = $(this).closest('.add-box');
+		var form = parent.children('.add-form');
+		var button = parent.children('.add-button');
+
+		$(button).removeClass('active');
+		$(form).removeClass('active');
+		form.children('input').val('');
+	});
+
+
 
 	bodyPadding();
+	sideBarHeight();
 
 	$(window).resize(function() {
 		bodyPadding();
+		sideBarHeight();
 	});
 
 });
 
 function bodyPadding() {
 	$('body').css('padding-bottom', $('.footer').height() + 'px');
+}
+
+function sideBarHeight() {
+	$('.sidebar').css('height', $('html').height() - $('.top-line').height() - $('.menu').height() - $('.progress-info').height() - $('.footer').height() + 'px');
 }
