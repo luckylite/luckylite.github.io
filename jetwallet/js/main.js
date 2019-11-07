3dbc6121d0b4a86eeb4989b6dc58c4dc32a78363
Vlad Kandyba <quit3.it@gmail.com>
2019-11-07 17:16:46 +0200

    asdasd

--- jetwallet/js/main.js
+++ jetwallet/js/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
     $('.form__multiple-inputs input').keyup(function() {
         var length = $(this).val().length;
-        var id = parseInt($(this).attr('data-input-id')) + 1;
+        var id = parseInt($(this).attr('data-input-id'));
         var parent = $(this).parent();
         
-        parent.children('input[data-input-id="' + id + '"]').focus();
+        if (length == 1) {
+            parent.children('input[data-input-id="' + (id + 1) + '"]').focus();
+        } else {
+            parent.children('input[data-input-id="' + (id - 1) + '"]').focus();
+        }
     });
 
     $('.login__img').css('min-width', ($(window).width() / 2 + 200) + 'px');
+
+    $('.mobile-button').click(function() {
+        $(this).toggleClass('mobile-button--opened');
+        $('.mobile-menu').toggleClass('mobile-menu--opened');
+    });
 });
