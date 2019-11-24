b5f3407057f7a57fbe62b93367acb1085ea11ae5
Vlad Kandyba <quit3.it@gmail.com>
2019-11-24 14:16:05 +0200

    jetwallet

--- jetwallet/js/dash.js
+++ jetwallet/js/dash.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $('.notifications-icon').click(function() {
+    $('.dash .notifications-icon').click(function() {
         $('.dash__notifications-bar').toggleClass('showed');
         $('.dash__right-bar').toggleClass('hidden');
     });
@@ -8,6 +8,18 @@ $(document).ready(function() {
     $('.dash__notifications-bar .notifics-hide').click(function() {
         $('.dash__notifications-bar').removeClass('showed');
         $('.dash__right-bar').removeClass('hidden');
+    });
+
+    $('.mobile-header .mobile-button').click(function() {
+        $(this).toggleClass('mobile-button--opened');
+        $('.mobile-header').toggleClass('mobile-header--opened');
+        $('.mobile-menu-dash').toggleClass('mobile-menu--opened');
+    });
+
+    $('.mobile-header .notifications-icon').click(function() {
+        $(this).toggleClass('opened');
+        $('.mobile-menu-dash .mobile-menu__notifics').toggleClass('showed');
+        $('.mobile-menu-dash .mobile-menu__accounts').toggleClass('hidden');
     });
 
 });
